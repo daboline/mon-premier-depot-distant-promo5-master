7b80e577cf5f204ffdec55d02d64d273d54bfa43
daboline <boudabi89@hotmail.fr>
2018-09-06 13:18:18 +0000

    Codage du fichier Employe.java

---
+++ Employe.java
@@ -0,0 +1,64 @@
+public class Employe{
+
+//Proprietes
+
+	private String nomEmploye;
+	private String prenomEmploye;
+	
+	//Constructeurs
+	
+	public Employe(){
+		
+	}
+	
+	public Employe(String nom, String prenom){
+		
+		this.nomEmploye=nom;
+		
+		this.prenomEmploye=prenom;
+		
+	}
+	
+	public Employe(String nom){
+		
+		this.nomEmploye=nom;
+		
+	}
+	
+	
+	
+	//Getters et setters
+	
+	public String getNomEmploye(){
+		
+		return this.nomEmpoye;
+	}
+	
+	public String getPrenomEmploye(){
+		
+		return this.prenomEmpoye;
+	}
+	
+	
+	public void setNomEmploye(String nom){
+		this.nomEmploye=nom;
+	}
+	
+	public void setPrenomEmploye(String prenom){
+		this.prenomEmploye=prenom;
+	}
+	
+	
+	//Ajout de methode metier
+	
+	public void afficherInfo(){
+		
+		System.out.println(this.nomEmploye + "" + this.prenomEmploye);
+		
+	}
+		
+	
+	
+	
+	
+}
