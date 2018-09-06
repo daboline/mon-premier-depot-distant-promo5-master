17f9c0772b544cf3385696e6a4c90ce00d9c6f15
daboline <boudabi89@hotmail.fr>
2018-09-06 13:23:01 +0000

    Codage du fichier Etudiant.java

---
+++ Etudiant.java
@@ -0,0 +1,54 @@
+public class Etudiant{
+
+//Proprietes
+
+	private String nomEtudiant;
+	private String prenomEtudiant;
+	
+	private String emailEtudiant;
+	
+	private String telEtudiant;
+	
+	//Constructeurs
+	
+	public Etudiant(){
+		
+	}
+	
+	public Etudiant(String nom, String prenom){
+		
+		this.nomEtudiant=nom;
+		
+		this.prenomEtudiant=prenom;
+		
+	}
+	
+	//Getters et setters
+	
+	public String getNomEtudiant(){
+		
+		return this.nomEtudiant;
+	}
+	
+	public String getPrenomEtudiant(){
+		
+		return this.prenomEtudiant;
+	}
+	
+	
+	public void setNomEtudiant(String nom){
+		this.nomEtudiant=nom;
+	}
+	
+	public void setPrenomEtudiant(String prenom){
+		this.prenomEtudiant=prenom;
+	}
+	
+	
+	//Ajout de methode metier
+	
+	public void afficherInfo(){
+		
+		System.out.println(this.nomEtuidiant + "" + this.prenomEtudiant);
+		
+	}
